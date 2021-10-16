6944adf661bfbb966799e5d742506c8a5ead6b29
remton <68307153+remton@users.noreply.github.com>
2021-10-16 18:56:20 -0400

    added zombiehealth script

---
+++ TopDownHordeGame_UnityProject/Assets/ZombieHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    private int health;
+
+    private void Start() {
+        health = maxHealth;
+    }
+
+    public void Damage(int amount) {
+        health -= amount;
+        if (health <= 0)
+            Die();
+    }
+
+    public void Heal(int amount) {
+        health += amount;
+        if (health > maxHealth)
+            health = maxHealth;
+    }
+
+    private void Die() {
+        Debug.Log(name + ": \"*dies\"");
+        Destroy(gameObject);
+    }
+}
