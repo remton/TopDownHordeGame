5653bc60fed9b5665c2806f50a02cf453c2514b3
remton <68307153+remton@users.noreply.github.com>
2021-11-04 16:22:33 -0400

    Added a shotgun (Remington870)

--- TopDownHordeGame_UnityProject/Assets/Scripts/Remington870.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Remington870.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class M1Garand : Weapon
+public class Remington870 : Weapon
 {
     public FireEffectController trail;
-    public override void Fire(GameObject player, Vector2 direction) {
-        base.Fire(player, direction);
+    public int pelletCount;
+    public float spreadAngle;
+
+    private void FireShot(GameObject player, Vector2 direction ) {
         RaycastHit2D effectRay = Physics2D.Raycast(player.transform.position, direction, Mathf.Infinity, LayerMask.GetMask("Wall"));
         Vector2 startPos = new Vector3(player.transform.position.x, player.transform.position.y, 0);
         if (effectRay) {
@@ -25,6 +27,19 @@ public class M1Garand : Weapon
             if (zombieHit.CompareTag("Zombie")) {
                 zombieHit.GetComponent<ZombieHealth>().Damage(damage);
             }
+        }
+    }
+
+    public override void Fire(GameObject player, Vector2 direction) {
+        base.Fire(player, direction);
+        direction.Normalize();
+        float baseAngle = Mathf.Atan2(direction.y, direction.x);
+        float angleDiff = -spreadAngle / 2;
+        Vector2 fireDir;
+        for (int i = 0; i < pelletCount; i++) {
+            fireDir = new Vector2(Mathf.Cos(baseAngle + angleDiff*Mathf.Deg2Rad), Mathf.Sin(baseAngle + angleDiff*Mathf.Deg2Rad));
+            FireShot(player, fireDir);
+            angleDiff += spreadAngle / (pelletCount - 1);
         }
     }
 }
