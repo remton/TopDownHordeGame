f81bedae9a09febea9ae16b901cb0b075543c597
remton <68307153+remton@users.noreply.github.com>
2021-11-19 19:29:44 -0500

    Added a playerActivate class with an event for when the player uses the activate button

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Player/PlayerActivate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActivate : MonoBehaviour
+{
+    public delegate void Activate();
+    public event Activate EventPlayerActivate;
+
+    public void OnActivateButton(InputAction.CallbackContext context) {
+        if (context.action.triggered && EventPlayerActivate != null) { }
+            EventPlayerActivate.Invoke();
+    }
+
+}
