1ba93f1077900d2433c4465501e9be9657845c0c
KickingPoet6015 <94138994+KickingPoet6015@users.noreply.github.com>
2021-12-04 13:36:29 -0500

    RPK Working

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/RPK.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPK : Weapon
+{
+    public int pelletCount;
+    public float spreadAngle;
+
+    public override void Fire(GameObject player, Vector2 direction)
+    {
+        base.Fire(player, direction);
+        direction.Normalize();
+        float baseAngle = Mathf.Atan2(direction.y, direction.x);
+        float angleDiff;
+        Vector2 fireDir;
+        angleDiff = Random.Range((baseAngle - spreadAngle / 2), (baseAngle + spreadAngle / 2));
+        fireDir = new Vector2(Mathf.Cos(baseAngle + angleDiff * Mathf.Deg2Rad), Mathf.Sin(baseAngle + angleDiff * Mathf.Deg2Rad));
+        FireShot(player, fireDir);
+    }
+}
