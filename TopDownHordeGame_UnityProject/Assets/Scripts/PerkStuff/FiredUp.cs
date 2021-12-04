862e17616236eb2a1c4b70de52cce225ee2d94f5
KickingPoet6015 <94138994+KickingPoet6015@users.noreply.github.com>
2021-12-04 11:59:08 -0500

    Fixed Duplicate File Issues

--- TopDownHordeGame_UnityProject/Assets/Scripts/PerkStuff/FiredUp.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/PerkStuff/FiredUp.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class FiredUp : Perk
 {
-    private float balance = 1.8F; // Multiplier for fireRate 
+    private float balance = .75F; // Multiplier for fireRate 
     //This is where the perk activates. This changes the regen values of the player.
     public override void OnPerkGained(GameObject player)
     {
         Debug.Log("Perk: " + name + " gained"); 
-        player.GetComponent<PlayerWeaponControl>().fireRateMult = (1 / balance);
+        player.GetComponent<PlayerWeaponControl>().fireRateMult = (balance);
     }
 
     //This is where the perk deactivates. This changes the regen values of the player.
     public override void OnPerkLost(GameObject player)
     {
         Debug.Log("Perk: " + name + " lost");
-        player.GetComponent<PlayerWeaponControl>().fireRateMult = (balance);
+        player.GetComponent<PlayerWeaponControl>().fireRateMult = (1 / balance);
     }
 }
 
