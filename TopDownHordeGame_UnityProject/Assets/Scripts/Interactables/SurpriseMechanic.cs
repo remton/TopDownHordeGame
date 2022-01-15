533205174abeca0996829a758f4731bad04741a9
KickingPoet6015 <94138994+KickingPoet6015@users.noreply.github.com>
2022-01-15 14:56:04 -0500

    Surprise Mechanic
    Surprise Mechanic is working. When new weapons are added, they will need to be manually added to the list located on the Surprise Mechanic object to be able to be given out.

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Interactables/SurpriseMechanic.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurpriseMechanic : MonoBehaviour
+{
+    public GameObject[] weaponPrefabList;
+    public GameObject popupCanvas;
+    public int cost;
+    private int backupCost;
+
+    public void TryBuyWeapon(GameObject player)
+    {
+        PlayerStats playerStats = player.GetComponent<PlayerStats>();
+        PlayerWeaponControl weaponControl = player.GetComponent<PlayerWeaponControl>();
+        if (playerStats.GetBank() >= cost)
+        {
+            playerStats.SpendMoney(cost);
+            int i = Mathf.FloorToInt(Random.Range(0, weaponPrefabList.Length));
+            weaponControl.PickUpWeapon(weaponPrefabList[i]);
+        }
+        else
+        {
+            Debug.Log("Need something. Money, perhaps.");
+        }
+    }
+
+    // Hitbox and player activation set
+    private HitBoxController hitbox;
+
+    private void Awake()
+    {
+        hitbox = GetComponent<HitBoxController>();
+        hitbox.EventObjEnter += OnPlayerEnter;
+        hitbox.EventObjExit += OnPlayerExit;
+        popupCanvas.SetActive(false);
+    }
+
+    public void OnPlayerEnter(GameObject player)
+    {
+        player.GetComponent<PlayerActivate>().EventPlayerActivate += TryBuyWeapon;
+        popupCanvas.SetActive(true);
+        popupCanvas.GetComponentInChildren<Text>().text = "Surprise Mechanic" + "\n$" + cost; // "Myster Box"
+    }
+    public void OnPlayerExit(GameObject player)
+    {
+        player.GetComponent<PlayerActivate>().EventPlayerActivate -= TryBuyWeapon;
+        popupCanvas.SetActive(false);
+    }
+    public void SaleStart(float price)
+    {
+        backupCost = cost;
+        Debug.Log("Price before: " + cost);
+        cost = Mathf.FloorToInt(cost * price);
+        Debug.Log("Price now: " + cost);
+    }
+    public void SaleEnd()
+    {
+        cost = backupCost;
+        Debug.Log("Price after end: " + cost);
+    }
+
+}
