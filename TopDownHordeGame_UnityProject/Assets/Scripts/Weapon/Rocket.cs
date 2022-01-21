0a8313666d09055a0b8925eea14abad6e294ba84
remton <68307153+remton@users.noreply.github.com>
2022-01-20 22:04:58 -0500

    Cleaned up rocket code removing old comments and unused variables
    NOTE: to change the radius of the RPG explosion just change the scale of the RPG explosion prefab

--- TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/Rocket.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/Rocket.cs
@@ -7,13 +7,10 @@ public class Rocket : MonoBehaviour
     public GameObject explosionPrefab;
     private GameObject explosionObj;
     private int balanceDamage;
-    private float balanceRadius;
     private float throwStrength;
     private float flySpeed;
     private Vector3 rotationTemp;
     private Vector2 moveDir;
-    private float timeActive;
-    [SerializeField] private float timeUntilDestroy;
 
     private GameObject owner;
 
@@ -26,7 +23,6 @@ public class Rocket : MonoBehaviour
         transform.rotation = Quaternion.Euler(rotationTemp);
         moveDir = movementDir;
         balanceDamage = damage;
-        balanceRadius = radius;
         throwStrength = knockback;
         flySpeed = speed;
         this.GetComponent<HitBoxController>().EventObjEnter += Explode;
@@ -36,31 +32,15 @@ public class Rocket : MonoBehaviour
     {
         Move(moveDir);
     }
-    //private void Update() {
-    //    if (explosionObj) {
-    //        if (timeActive >= timeUntilDestroy) {
-    //            DestroyExplosionObject(explosionObj);
-    //            timeActive = 0;
-    //        }
-    //        else {
-    //            timeActive += Time.deltaTime;
-    //        }
-    //    }
-    //}
+  
     //Creates an Explosion Object
     public void Explode(GameObject player)
     {
         Debug.Log("Creating explosion object.");
-        timeActive = 0;
         //Stop this from exploding multiple times
         GetComponent<HitBoxController>().EventObjEnter -= Explode;
         Vector3 location = transform.position;
         explosionObj = Instantiate(explosionPrefab, location, Quaternion.identity);
-        
-        //Vector3 balanceScale;
-        //balanceScale.x = balanceRadius;
-        //balanceScale.y = balanceRadius;
-        //balanceScale.z = balanceRadius;
 
         List<string> damageTags = new List<string>();
         damageTags.Add("Zombie");
@@ -70,13 +50,6 @@ public class Rocket : MonoBehaviour
 
         explosionObj.GetComponent<Explosion>().Init(owner, damageTags, knockbackTags, balanceDamage, throwStrength);
         Destroy(gameObject);
-
-        //HideRocketObject();
-
-        //Debug.Log("Resizing explosion object.");
-        //explosionObj.transform.localScale = balanceScale;
-        //explosionObj.GetComponent<HitBoxController>().EventObjEnter += DamageZombies;
-        //explosionObj.GetComponent<HitBoxController>().EventObjEnter += Knockback;
     }
 
     [SerializeField] private Rigidbody2D rb;
@@ -85,42 +58,5 @@ public class Rocket : MonoBehaviour
         newPos += flySpeed * movementDir * Time.fixedDeltaTime;
         rb.MovePosition(newPos);
     }
-    //private void HideRocketObject()
-    //{
-    //    Vector3 hide; 
-    //    this.GetComponent<HitBoxController>().EventObjEnter -= DamageZombies;
-    //    flySpeed = 0;
-    //    hide.x = transform.position.x * 10;
-    //    hide.y = transform.position.y * 10;
-    //    hide.z = transform.position.z * 10;
-    //    transform.position = hide;
-    //    Debug.Log("Rocket should be hidden.");
-    //}
-
-    //private void DestroyExplosionObject(GameObject explosionObj)
-    //{
-    //    this.GetComponent<HitBoxController>().EventObjEnter -= DamageZombies;
-    //    this.GetComponent<HitBoxController>().EventObjEnter -= Knockback;
-    //    Destroy(explosionObj);
-    //    Destroy(this);
-    //}
-    //public void DamageZombies(GameObject zombie)
-    //{
-    //    if (zombie.CompareTag("Zombie"))
-    //    {
-    //        zombie.GetComponent<ZombieHealth>().Damage(balanceDamage);
-    //        owner.GetComponent<PlayerStats>().AddMoney(1); // Give the player money for the explosion hitting someone 
-    //        if (zombie.GetComponent<ZombieHealth>().isDead())
-    //            owner.GetComponent<PlayerStats>().AddKill();
-    //    }
-    //}
-    //public void Knockback(GameObject creature)
-    //{
-    //    Vector2 epicenterLocation = explosionObj.transform.position;
-    //    Vector2 creatureLocation = creature.transform.position;
-    //    Vector2 throwDirection = creatureLocation - epicenterLocation;
-    //    throwDirection.Normalize();
-    //    Vector2 throwAmount = throwDirection * throwStrength;
-    //    creature.GetComponent<Rigidbody2D>().AddForce(throwAmount);
-    //}
+   
 }
