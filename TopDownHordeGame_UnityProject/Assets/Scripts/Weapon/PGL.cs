a5b930643b84c367dd58622a6859becb79dfa97c
KickingPoet6015 <94138994+KickingPoet6015@users.noreply.github.com>
2022-01-23 17:05:19 -0500

    SGL
    A Sticky Grenade Launcher (SGL) shoots sticky grenades. They attach to whatever they impact. They explode a set time after they are shot.

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/PGL.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PGL : Weapon 
+{
+    [SerializeField] protected float balanceRadius = 5F; // Radius for grenade 
+    protected float knockback = 600F; 
+    protected float flySpeed = 30F; 
+    public GameObject grenadeObjPrefab;
+    public GameObject player;
+    private Vector3 playerPos;
+    protected Vector2 playerLookDir;
+    protected float explodeTime = 1.5f;
+
+    public override void Fire(GameObject player, Vector2 direction)
+    {
+        base.Fire(player, direction);
+        FireGrenade(player, direction);
+    }
+
+    public void FireGrenade(GameObject player, Vector2 direction)
+    {
+        Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        playerLookDir = direction;
+        GameObject grenade = Instantiate(grenadeObjPrefab, playerPos, Quaternion.identity);
+        grenade.GetComponent<Grenade>().Init(player, playerLookDir.normalized, damage, balanceRadius, flySpeed, knockback, explodeTime);
+    }
+}
---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/StickyGrenade.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyGrenade : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    private GameObject explosionObj;
+    private int balanceDamage;
+    private float throwStrength;
+    private float flySpeed;
+    private Vector3 rotationTemp;
+    private Vector2 moveDir;
+    private float balanceTimer;
+    private Timer timer;
+    private bool stuck;
+    private bool stuckToScenery;
+    private GameObject stuckOn;
+    Vector3 location;
+
+    private GameObject owner;
+
+    public void Init(GameObject newOwner, Vector2 movementDir, int damage, float radius, float speed, float knockback, float explodeTime)
+    {
+        owner = newOwner; 
+        transform.position = newOwner.transform.position;
+        rotationTemp = newOwner.transform.rotation.eulerAngles;
+        rotationTemp.z += 90;
+        transform.rotation = Quaternion.Euler(rotationTemp);
+        moveDir = movementDir;
+        balanceDamage = damage;
+        throwStrength = knockback;
+        flySpeed = speed;
+        balanceTimer = explodeTime;
+        stuck = false;
+        stuckToScenery = false;
+        timer = GetComponent<Timer>();
+        gameObject.GetComponent<HitBoxController>().EventObjEnter += Stick;
+        timer.CreateTimer(balanceTimer, Explode);
+    }
+
+    private void FixedUpdate()
+    {
+        Move(moveDir);
+        if (stuck && !stuckToScenery)
+        {
+            //if () 
+            //{
+                location = stuckOn.transform.position;
+            //}
+            transform.position = location;
+        }
+    }
+  
+    //Creates an Explosion Object
+    public void Explode(/*GameObject player*/)
+    {
+        Debug.Log("Creating explosion object.");
+        location = transform.position;
+        explosionObj = Instantiate(explosionPrefab, location, Quaternion.identity);
+
+        List<string> damageTags = new List<string>();
+        damageTags.Add("Zombie");
+        List<string> knockbackTags = new List<string>();
+        knockbackTags.Add("Zombie");
+        knockbackTags.Add("Player");
+
+        explosionObj.GetComponent<Explosion>().Init(owner, damageTags, knockbackTags, balanceDamage, throwStrength);
+        Destroy(gameObject); 
+    }
+    // Sticks to anything the grenade impacts. 
+    public void Stick(GameObject objectHit)
+    {
+        flySpeed = 0;
+        stuck = true;
+        if (!(objectHit.CompareTag("Player") || objectHit.CompareTag("Zombie"))) {
+            stuckToScenery = true;
+        }
+        else
+        {
+            stuckOn = objectHit;
+        }
+    }
+
+    [SerializeField] private Rigidbody2D rb;
+    private void Move(Vector2 movementDir) {
+        Vector2 newPos = transform.position;
+        newPos += flySpeed * movementDir * Time.fixedDeltaTime;
+        rb.MovePosition(newPos);
+    }
+   
+}
