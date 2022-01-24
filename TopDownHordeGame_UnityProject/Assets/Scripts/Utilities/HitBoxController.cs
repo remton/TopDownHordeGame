0c4476f21380bb0073cd239ea8d7735b1d84fcc5
remton <68307153+remton@users.noreply.github.com>
2022-01-24 11:09:34 -0500

    I commented some utility scripts that will probably be used in future projects

--- TopDownHordeGame_UnityProject/Assets/Scripts/Utilities/HitBoxController.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Utilities/HitBoxController.cs
@@ -1,17 +1,33 @@
+/*HitBoxController.cs
+ * Author: Remington Ward
+ * Date: 1/24/2022
+ * This script is built for the Unity Game Engine. It provides an easy funcionality for other scripts to have a hitbox
+ * Any object with this script must have a collider set to trigger that will collide with all needed objects
+ * This script has a list of tags (triggerTags) that this hitbox will be affectd by.
+ * When an object enters the event EventObjEnter will be onvoked. EventObjExit will likewise be invoked on an objects exit. 
+ * These events will pass the object as a parameter.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HitBoxController : MonoBehaviour {
-    [Header("Capital sensative tags")]
-    public List<string> triggerTags = new List<string>(); // Events only triggered when an objet with one of these tags is detected
-    private bool active = true;
-    private List<GameObject> objsInBox = new List<GameObject>();
+    // Events only triggered when an object with one of these tags is detected
+    [Header("Capital sensative tags!")]
+    public List<string> triggerTags = new List<string>(); 
 
+    //Events for when an object enters and exits (remember to unsubscribe if necessary!)
+    public delegate void ObjEnter(GameObject obj);
+    public event ObjEnter EventObjEnter;
+    public delegate void ObjExit(GameObject obj);
+    public event ObjExit EventObjExit;
+
+    //If this hitbox should currently be working
     public void SetActive(bool b) { active = b; }
     public bool GetActive() { return active; }
 
-    /// <summary>Calls EventObjEnter for every obj already in the hitbox that has an active tag</summary>
+    /// <summary> Calls EventObjEnter for every obj already in the hitbox that has an active tag</summary>
     public void ForceEntry() {
         foreach (GameObject obj in objsInBox) {
             if (obj != null) {
@@ -20,24 +36,23 @@ public class HitBoxController : MonoBehaviour {
             }
         }
     }
-
+    /// <summary> Returns all current objects in the hitbox </summary>
     public List<GameObject> Hits() {
         return objsInBox;
     }
 
-    public delegate void ObjEnter(GameObject obj);
-    public event ObjEnter EventObjEnter;
+    // ----- Private -----
 
+    private bool active = true;
+    private List<GameObject> objsInBox = new List<GameObject>();
+
+    // These functions are called by unity when a gameObject enters and exits the trigger collider respectively
     private void OnTriggerEnter2D(Collider2D collision) {
         objsInBox.Add(collision.gameObject);
         if (!active) return;
         if (Utilities.CompareTags(collision.gameObject, triggerTags))
             if (EventObjEnter != null) { EventObjEnter.Invoke(collision.gameObject); }
     }
-
-    public delegate void ObjExit(GameObject obj);
-    public event ObjExit EventObjExit;
-
     private void OnTriggerExit2D(Collider2D collision) {
         objsInBox.Remove(collision.gameObject);
         if (!active) return;
--- TopDownHordeGame_UnityProject/Assets/Scripts/Utilities/Timer.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Utilities/Timer.cs
@@ -1,3 +1,12 @@
+/* Timer.cs
+ * Author: Remington Ward
+ * Date: 1/24/2022
+ * This script is built for the Unity Game Engine. It provides a simple way to wait an amount of time for any script.
+ * Just add the Timer script to the object requiring some wait time, the with a reference to it ie. Timer timer
+ * call timer.CreateTimer(float time, Action methodOnTimerEnd);
+ * The method for the timer's end must have no arguments and return void
+ */
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,20 +15,7 @@ using UnityEngine;
 // Add a timer component to your script and make as many timers as you want :D
 public class Timer : MonoBehaviour
 {
-    class SingleTimer {
-        public Action onEnd;
-        public float timeLeft;
-        public bool isTimerRunning;
-    }
-
-    private List<SingleTimer> timers;
-
-    private void Awake() {
-        timers = new List<SingleTimer>();
-    }
-
     public void CreateTimer(float time, Action onEnd) {
-
         SingleTimer newTimer = new SingleTimer();
         newTimer.onEnd = onEnd;
         newTimer.timeLeft = time;
@@ -27,10 +23,15 @@ public class Timer : MonoBehaviour
         timers.Add(newTimer);
     }
 
+    private class SingleTimer {
+        public Action onEnd;
+        public float timeLeft;
+        public bool isTimerRunning;
+    }
+    private List<SingleTimer> timers = new List<SingleTimer>();
     private void RemoveTimer(int index) {
         timers.RemoveAt(index);
     }
-
     private void Update(){
         for (int i = 0; i < timers.Count; i++) {
             if (timers[i].isTimerRunning) {
@@ -41,12 +42,5 @@ public class Timer : MonoBehaviour
                 }
             }
         }
-    }
-
-    private void OnDestroy() {
-        //for (int i = 0; i <  numTimers; i++) {
-        //    actionsOnEnd[i]();
-        //    RemoveTimer(i);
-        //}
     }
 }
