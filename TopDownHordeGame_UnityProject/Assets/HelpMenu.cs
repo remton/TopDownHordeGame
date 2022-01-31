b3b24235f95a4d5e5126e91e04505f9049864a34
remton <68307153+remton@users.noreply.github.com>
2022-01-31 12:06:41 -0500

    Help menu structure done

---
+++ TopDownHordeGame_UnityProject/Assets/HelpMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HelpMenu : MonoBehaviour
+{
+    GameObject currOverlay;
+    public void SetOverlay(GameObject overlay) {
+        overlay.SetActive(true);
+        currOverlay = overlay;
+    }
+    public void DisableOverlay() {
+        currOverlay.SetActive(false);
+    }
+    public void BackToPregame() {
+        //Pregame scene should be index 0
+        SceneManager.LoadScene(0);
+    }
+}
--- TopDownHordeGame_UnityProject/Assets/Scripts/UI_Menus/PreGameController.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/UI_Menus/PreGameController.cs
@@ -9,13 +9,14 @@ public class PreGameController : MonoBehaviour
     public int maxPlayers;
     public int numPlayers;
     public Text numPlayerTxt;
-    public string sceneToLoad;
 
     private void Awake() {
         Cursor.visible = true; // Make sure we can see the cursor since it is made invisible and replaced by the reticle during gameplay
     }
 
     private void Start() {
+        numPlayerTxt.text = numPlayers.ToString();
+        numPlayers = GameSettings.instance.numPlayers;
         numPlayerTxt.text = numPlayers.ToString();
     }
 
@@ -24,21 +25,23 @@ public class PreGameController : MonoBehaviour
             numPlayers++;
             numPlayerTxt.text = numPlayers.ToString();
         }
+        GameSettings.instance.numPlayers = numPlayers;
     }
     public void DecreaseNumPlayers() {
         if (numPlayers > 1) {
             numPlayers--;
             numPlayerTxt.text = numPlayers.ToString();
         }
+        GameSettings.instance.numPlayers = numPlayers;
     }
 
     public void StartGame() {
-        GameSettings.instance.numPlayers = numPlayers;
-        SceneManager.LoadScene(sceneToLoad);
+        //Loads cat cafe index 2
+        SceneManager.LoadScene(2);
     }
     public void HelpMenu() {
-        return;
         GameSettings.instance.numPlayers = numPlayers;
-        SceneManager.LoadScene("HelpMenu");
+        //Help menu index 1
+        SceneManager.LoadScene(1);
     }
 }
