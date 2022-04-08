71ab291669b0d9ebe91d70712fbd800c2049f220
elykins <ericl_47@yahoo.com>
2022-04-08 01:26:19 -0400

    G18 Added
    G18 (fully automatic pistol) was added. It seems to be fully implemented with the exception of waiting on sprites.

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/G18.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G18 : Weapon
+{
+    public int pelletCount;
+    public float spreadAngle;
+
+    public override void Fire(GameObject player, Vector2 direction)
+    {
+        base.Fire(player, direction);
+        FireShot(player, direction, spreadAngle);
+    }
+}
