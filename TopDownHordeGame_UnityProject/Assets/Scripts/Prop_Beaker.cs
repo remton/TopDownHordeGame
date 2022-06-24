943784ff938b768d5d96ac81159c58a5564f2540
Remington Ward <68307153+remton@users.noreply.github.com>
2022-06-24 11:27:19 -0400

    Created a beaker prop

---
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Prop_Beaker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Timer))]
+public class Prop_Beaker : Prop
+{
+    [SerializeField] private float destroyTime;
+    [SerializeField] private Animator myAnimator;
+    [SerializeField] private Animator liquidAnimator; 
+    public AudioClip breakSound;
+    protected override void OnShot() {
+        canBeShot = false;
+        AudioManager.instance.PlaySound(breakSound);
+        myAnimator.SetTrigger("shot");
+        liquidAnimator.SetTrigger("shot");
+        TimedDestroy td = gameObject.AddComponent<TimedDestroy>();
+        td.destroyTime = destroyTime;
+    }
+}
