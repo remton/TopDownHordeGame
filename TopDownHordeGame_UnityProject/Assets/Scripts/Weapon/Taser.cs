e9b9594539ef1c0a1e17b1b7dc5855b9971b3628
Remington Ward <68307153+remton@users.noreply.github.com>
2022-08-17 23:34:20 -0400

    Added a taser weapon that utilizes the chain electricity

--- TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/Taser.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/Taser.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Remington870 : Weapon
+[RequireComponent(typeof(ElectricfyWeapon))]
+public class Taser : Weapon
 {
-    public int pelletCount;
-    public float spreadAngle;
-
+    [SerializeField] private float spreadAngle;
     public override void Fire(GameObject player, Vector2 direction) {
         base.Fire(player, direction);
         direction.Normalize();
         float baseAngle = Mathf.Atan2(direction.y, direction.x);
         float angleDiff = -spreadAngle / 2;
         Vector2 fireDir;
-        for (int i = 0; i < pelletCount; i++) {
-            fireDir = new Vector2(Mathf.Cos(baseAngle + angleDiff*Mathf.Deg2Rad), Mathf.Sin(baseAngle + angleDiff*Mathf.Deg2Rad));
+        for (int i = 0; i < 2; i++) {
+            fireDir = new Vector2(Mathf.Cos(baseAngle + angleDiff * Mathf.Deg2Rad), Mathf.Sin(baseAngle + angleDiff * Mathf.Deg2Rad));
             FireShot(player, fireDir);
-            angleDiff += spreadAngle / (pelletCount - 1);
+            angleDiff += spreadAngle;
         }
     }
 }
