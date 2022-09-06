34f6ce7fea7cab6df5181e665f8b5e665f417544
J-Se <jse@bex.net>
2022-09-05 20:12:25 -0400

    BGL mostly working
    Grenades sometimes stop instead of bouncing.

--- TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/MiscWeaponStuff/Grenade.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Weapon/MiscWeaponStuff/Grenade.cs
@@ -12,12 +12,14 @@ public class Grenade : MonoBehaviour
     private Vector3 rotationTemp;
     private Vector2 moveDir;
     private float balanceTimer;
+    [SerializeField] private float dragCoefficient;
     private Timer timer;
 
     private GameObject owner;
 
     public void Init(GameObject newOwner, Vector2 movementDir, float damage, float radius, float speed, float knockback, float explodeTime)
     {
+        //Debug.Log("Grenade Init ran");
         owner = newOwner; 
         transform.position = newOwner.transform.position;
         rotationTemp = newOwner.transform.rotation.eulerAngles;
@@ -31,6 +33,7 @@ public class Grenade : MonoBehaviour
         timer = GetComponent<Timer>();
         gameObject.GetComponent<HitBoxController>().EventObjEnter += Ricochet;
         timer.CreateTimer(balanceTimer, Explode);
+        //Debug.Log("Grenade Init ended");
     }
 
     private void FixedUpdate()
@@ -41,16 +44,16 @@ public class Grenade : MonoBehaviour
     //Creates an Explosion Object
     public void Explode(/*GameObject player*/)
     {
-        Debug.Log("Creating explosion object.");
+        //Debug.Log("Creating explosion object.");
         //Stop this from exploding multiple times
         //GetComponent<HitBoxController>().EventObjEnter -= Explode;
         Vector3 location = transform.position;
         explosionObj = Instantiate(explosionPrefab, location, Quaternion.identity);
 
         List<string> damageTags = new List<string>();
-        damageTags.Add("Zombie");
+        damageTags.Add("ZombieDamageHitbox");
         List<string> knockbackTags = new List<string>();
-        knockbackTags.Add("Zombie");
+        knockbackTags.Add("ZombieDamageHitbox");
         knockbackTags.Add("Player");
 
         explosionObj.GetComponent<Explosion>().Init(owner, damageTags, knockbackTags, balanceDamage, throwStrength);
@@ -60,13 +63,30 @@ public class Grenade : MonoBehaviour
     private void Ricochet(GameObject objectHit)
     {
         // Reflecting Bullets
+        //Debug.Log("Grenade Ricochet ran");
+        string[] mask = { "BulletCollider", "ZombieHitbox", "Door" };
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, moveDir, 3, LayerMask.GetMask(mask)); // Raycasting is okay here, since this function is only called during collision
+        Vector2 hitPoint = hitInfo.point;
+        Debug.Log("normal vector: " + hitInfo.normal);
+        //Vector2 reflectedVector = Vector2.Reflect(moveDir, hitInfo.normal);
+        Vector2 position2d = transform.position;
+        Vector2 reflectedVector = Vector2.Reflect((hitPoint - position2d).normalized, hitInfo.normal);
+        moveDir = reflectedVector;
+        Debug.Log("final move direction: " + reflectedVector);
+        flySpeed *= 0.8f;
+
+        /*
         Quaternion normal = objectHit.transform.rotation;
         Vector2 normalVector = normal.eulerAngles;
         Vector2 v = Vector2.Reflect(transform.up, normalVector);
+        Debug.Log("moveDir = " + moveDir);
+        Debug.Log("normalVector = " + normalVector);
+        Debug.Log("reflected vector = " + v);
         float rot = Mathf.Atan2(-v.x, v.y) * Mathf.Rad2Deg;
-
         transform.eulerAngles = new Vector3(0, 0, rot);
         moveDir = transform.eulerAngles;
+        Debug.Log("moveDir = " + moveDir);
+        */
     }
 
     [SerializeField] private Rigidbody2D rb;
@@ -75,5 +95,10 @@ public class Grenade : MonoBehaviour
         newPos += flySpeed * movementDir * Time.fixedDeltaTime;
         rb.MovePosition(newPos);
     }
-   
+
+    private void Update()
+    {
+        flySpeed *= dragCoefficient;
+    }
+
 }
