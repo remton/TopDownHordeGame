bded3f113a1889a16f9a2052ba10c0e621e50fdd
remington ward <remton2001@gmail.com>
2022-11-11 20:01:43 -0500

    Added Tube prop (unfinished)

---
+++ TopDownHordeGame_UnityProject/Assets/Prop_Tube.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop_Tube : Prop
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite brokenSprite;
+    public Sprite subjectSprite;
+    public AudioClip breakSound;
+    public GameObject fluidObj;
+    public GameObject subjectObj;
+    public bool hasSubject;
+    public bool broken;
+    public int durability;
+
+    private void Start() {
+        if (hasSubject) {
+            subjectObj.GetComponent<SpriteRenderer>().sprite = subjectSprite;
+            subjectObj.SetActive(true);
+        }
+        else {
+            subjectObj.SetActive(false);
+        }
+        if (broken) {
+            BreakTube();
+        }
+    }
+
+    private void BreakTube() {
+        canBeShot = false;
+        AudioManager.instance.PlaySound(breakSound);
+        spriteRenderer.sprite = brokenSprite;
+        fluidObj.SetActive(false);
+        if (subjectObj.activeSelf) {
+
+        }
+        subjectObj.SetActive(false);
+    }
+
+    protected override void OnShot() {
+        durability--;
+        if(durability <= 0) {
+            BreakTube();
+        }
+    }
+}
