4df39bbe1e2b964908b9dabbb0d63783164dafe9
EricLykins <elykins@bgsu.edu>
2023-03-23 16:14:57 -0400

    Working
    The challenge is now working. The modifier properly unlocks. The main menu's challenges break when the CSWS challenge is completed in a game.

--- TopDownHordeGame_UnityProject/Assets/Scripts/Challenges/Challenge_CSWS.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Challenges/Challenge_CSWS.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Timer))]
 public class Challenge_CSWS : Challenge
 {
-    private const float CSWS_UPDATE_TIME = .5f;
-    public bool roundComplete = true;
+    private const float CSWS_UPDATE_TIME = .5f; // Frequency of checks
+    private bool roundComplete = false; // Used to check if the player moved this round. Starts false, changes to true each round. Changes to flase when the player stops moving. 
     private Timer timer;
     public override void Load() {
         base.Load();
+        timer = GetComponent<Timer>(); // Timer initialized
         Debug.Log("CSWS Load()");
         Unlock(); // This is unlocked by default
     }
@@ -27,44 +29,61 @@ public class Challenge_CSWS : Challenge
         }
     }
 
-    private void CSWS_Check()
+    private void CSWS_Check() // Check if the challenge has been failed 
     {
-        Debug.Log("CHECKING Can't Stop Won't Stop");
+        Debug.Log("CHECKING CSWS");
 
         foreach (var player in PlayerManager.instance.GetActiveLocalPlayers())
         {
+            //Debug.Log("CSWS foreach");
             //If the player hasn't moved since the last check for CSWS
             if (player.GetComponent<PlayerMovement>().lastMoved > CSWS_UPDATE_TIME)
             {
-                Debug.Log("FAILED Can't Stop Won't Stop");
+                Debug.Log("FAILED CSWS");
                 roundComplete = false;
             }
+            //Debug.Log("CSWS after if");
         }
-        if(roundComplete && !completed)
+        if (roundComplete && !completed)
+        {
+            Debug.Log("CSWS resetting");
             timer.CreateTimer(CSWS_UPDATE_TIME, CSWS_Check);
+            Debug.Log("CSWS Timer restarted");
+        }
     }
     private void CSWS_RoundCheck(int round)
     {
-        if(round == 1)
+        if (!completed)
         {
-            CSWS_Check(); 
-            Debug.Log("First round check.");
-        }
-        else if (round > 1)
-        {
-            if(roundComplete)
+            //if (round == 1)
+            //{
+            //    roundComplete = true;
+            //    Debug.Log("CSWS Line 60");
+            //    CSWS_Check();
+            //    Debug.Log("First round CSWS check.");
+            //}
+            //else
+            if (round > 1)
             {
-                Complete();
-                Debug.Log("Completing Can't Stop Won't Stop");
+                if (roundComplete)
+                {
+                    Debug.Log("Completing CSWS");
+                    Complete();
+                }
             }
-            roundComplete = true;
-            CSWS_Check();
-            Debug.Log("Not completed. Restarting.");
+                roundComplete = true;
+                Debug.Log("Not completed. Restarting CSWS.");
+                CSWS_Check();
+            //}
+        }
+        else
+        {
+            RoundController.instance.EventRoundChange -= CSWS_RoundCheck;
         }
     }
     public override void Complete() {
         base.Complete();
         SaveData.instance.modifier_unlocks[(int)ModifierType.CSWSMovement] = true;
-        Debug.Log("Completed Can't Stop Won't Stop");
+        Debug.Log("Completed CSWS");
     }
 }
