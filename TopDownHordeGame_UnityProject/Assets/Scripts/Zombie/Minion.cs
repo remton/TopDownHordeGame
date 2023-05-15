986da3641f6a0f6f81ea50c5b16fa35e82a15870
Remington Ward <68307153+remton@users.noreply.github.com>
2023-05-14 22:00:59 -0400

    Added minion animations and updated minion ai to choose to have an item or not

--- TopDownHordeGame_UnityProject/Assets/Scripts/Zombie/Minion.cs
+++ TopDownHordeGame_UnityProject/Assets/Scripts/Zombie/Minion.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 public class Minion : BasicZombieAI
 {
     [HideInInspector] public GameObject owner;
     private bool useDeathEvent = false;
+    [SerializeField] private GameObject item;
 
     protected override void Start() {
         base.Start();
@@ -13,6 +15,8 @@ public class Minion : BasicZombieAI
             owner.GetComponent<ZombieHealth>().EventOnDeath += GetComponent<ZombieHealth>().Kill;
             useDeathEvent = true;
         }
+        if (isServer)
+            DecideItem();
     }
 
     protected override void OnDestroy() {
@@ -20,5 +24,19 @@ public class Minion : BasicZombieAI
         if (owner != null && useDeathEvent) {
             owner.GetComponent<ZombieHealth>().EventOnDeath -= GetComponent<ZombieHealth>().Kill;
         }
+    }
+
+    [Server]
+    private void DecideItem() {
+        if (item == null)
+            return;
+        int rand = Random.Range(0, 5);
+        bool hasFlag = (rand == 0);
+        SetItemActive(hasFlag);
+    }
+
+    [ClientRpc]
+    private void SetItemActive(bool b) {
+        item.SetActive(b);
     }
 }
